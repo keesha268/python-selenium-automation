07efdccddd3d8a4e1b0681338ce1a78e3ae5100b
Keesha <keesha406@gmail.com>
2023-03-20 23:00:34 -0400

    homework_5

--- features/steps/product_page_steps.py
+++ features/steps/product_page_steps.py
@@ -3,8 +3,27 @@ from behave import when, then
 from time import sleep
 
 ADD_TO_CART_BTN = (By.ID, 'add-to-cart-button')
+COLOR_OPTIONS = (By.CSS_SELECTOR, "#variation_color_name li")
+CURRENT_COLOR = (By.CSS_SELECTOR, "#variation_color_name .selection")
 
 @when('Click on Add to cart button')
 def click_add_to_cart(context):
     context.driver.find_element(*ADD_TO_CART_BTN).click()
     sleep(4)
+
+@given('Open Amazon product {product_id} page')
+def open_product(context, product_id):
+    context.driver.get(f'https://www.amazon.com/dp/{product_id}/')
+
+
+@then('Verify user can click through colors')
+def verify_user_can_select_colors(context):
+    context.driver.find_element(*COLOR_OPTIONS).click()     # This would only click 1 color
+
+    all_color_options = context.driver.find_elements(*COLOR_OPTIONS)
+    print('All colors:', all_color_options)
+
+    for color in all_color_options:
+        color.click()
+        current_color = context.driver.find_element(*CURRENT_COLOR).text
+        print('Current Color: ', current_color)
---
+++ features/tests/product_details.feature
@@ -0,0 +1,14 @@
+#Feature: Tests for product page
+#
+#
+#  Scenario: User can select colors
+#    Given Open Amazon product B08JHKQPBV page
+#    Then Verify user can click through colors
+
+
+Feature: Tests for product page
+
+
+  Scenario: Verify user can select various colors
+    Given Open Amazon product B0BRKXVZL7 page
+    Then Verify user can click through colors
